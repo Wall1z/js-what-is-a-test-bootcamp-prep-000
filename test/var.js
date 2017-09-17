bf6d83497b7e78411f2094acbb10369d1207aec2
wael alnajar <w_alnajar@yahoo.com>
2017-09-17 19:59:29 +0000

    automatically backed up by learn

--- test/var.js
+++ test/var.js
@@ -1,11 +1,12 @@
 
-var a = 5,
-    b = 2,
-    c = 3,
-    d = 'hello',
-    e = 'goodbye',
-    console.log(a)
-    console.log(b)
-    console.log(c)
-    console.log(d)
-    console.log(e)
+console.log('Hello, world!')
+console.log('Hello, world!')
+console.log('Hello, world!')
+console.log('Hello, world!')
+console.log('Hello, world!')
+console.log('Hello, world!')
+console.log('Hello, world!')
+console.log('Hello, world!')
+console.log('Hello, world!')
+console.log('Hello, world!')
+console.log('Hello, world!')
