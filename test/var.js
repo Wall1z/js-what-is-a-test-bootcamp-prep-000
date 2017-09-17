80bccc2a467b9f9bd175464b150a51c66431ab60
wael alnajar <w_alnajar@yahoo.com>
2017-09-17 19:41:10 +0000

    automatically backed up by learn

--- test/var.js
+++ test/var.js
@@ -1,3 +1,12 @@
 
-var word-"dird"
-console.log(word)
+var word
+ 
+// assign a value to the variable
+word = 'bird'
+ 
+console.log(word) // 'bird'
+ 
+// assign another value to the variable
+word = 'dog'
+ 
+console.log(word) // 'dog'
